e2b1e067da1b92cbad363c2f0bfc224acfa895de
freddie <freddiebutcher@gmail.com>
2019-07-27 13:26:30 +1000

    Login set up 1

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,24 @@
+class SessionController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by :email_address => params[:email_address]
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "User created!"
+      redirect_to root_path
+    else
+      flash[:error] = 'User could not be created!'
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_040752) do
+ActiveRecord::Schema.define(version: 2019_07_27_023020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_07_26_040752) do
     t.text "user_bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
