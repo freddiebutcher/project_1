d5ae3be2727fa187602f5701608f0f43076f3f38
freddie <freddiebutcher@gmail.com>
2019-07-27 17:10:46 +1000

    users set up2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,11 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-#
+
   def new
     @user = User.new
   end
-# #
+
   def create
       @user = User.new user_params
       if @user.save
@@ -29,17 +29,17 @@ class UsersController < ApplicationController
     user.update user_params
     redirect_to user
   end
-#
-#   def show
-#     @user = User.find params[:id]
-#   end
-#
-#   def destroy
-#     user = User.find params[:id]
-#     user.destroy
-#     redirect_to users_path
-#   end
-#
+
+  def show
+    @user = User.find params[:id]
+  end
+
+  def destroy
+    user = User.find params[:id]
+    user.destroy
+    redirect_to users_path
+  end
+
 private
   def user_params
     params.require(:user).permit(:email_address, :password, :password_confirmation, :username, :first_name, :last_name, :user_bio)
